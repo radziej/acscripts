62cbac891fb7551d77b4020123c4bfc1a9bd56c1
Markus Radizej <radziej@physik.rwth-aachen.de>
2015-04-29 14:24:12 +0200

    Updated to modified website layout
    The script can now read the latest layout with 3 lines of Pasta.
    It also allows for umlauts that appear without sourcing CMSSW.

--- mensascript.py
+++ mensascript.py
@@ -23,40 +23,50 @@ class Dish():
     def __init__(self, category):
         self.category   = category
         self.description= ""
-        self.price      = "0,00"
+        self.price      = ""
     def extract_price(self, line):
-        return re.search("[0-9],[0-9][0-9]", line).group()
+        return re.search("[0-9],[0-9][0-9] (\xe2\x82\xac|EUR)", line).group()
     def extract_description(self, line):
-        return re.sub(" \^(.(,[a-zA-Z0-9])*|\w+)", "", line.replace(" EUR", "")[len(self.category):-len(self.price)]).strip()
+        return re.sub(" \^(.(,[a-zA-Z0-9])*|\w+)", "", (line[len(self.category):-len(self.price)] if self.price else line[len(self.category):]).strip())
 
 
-# parse the websites information into each dish
-dishes = [Dish("Tellergericht"), Dish("Vegetarisch"), Dish("Empfehlung des Tages"), Dish("Klassiker"), Dish("Pizza des Tages"), Dish("Pasta"), Dish("Wok")]
-for dish in dishes:
+def main():
+    # parse the websites information into each dish
+    dishes = [Dish("Tellergericht"), Dish("Vegetarisch"), Dish("Empfehlung des Tages"), Dish("Klassiker"), Dish("Pizza des Tages"), Dish("Pasta"), Dish("Pasta"), Dish("Pasta"), Dish("Wok")]
+    i = 0
     for line in lines:
         sline = line.strip()
-        if sline.lower().startswith(dish.category.lower()):
-            dish.price = dish.extract_price(sline)
-            dish.description = dish.extract_description(sline)
-            break
+        if sline.lower().startswith(dishes[i].category.lower()):
+            dishes[i].price       = dishes[i].extract_price(sline)
+            dishes[i].description = dishes[i].extract_description(sline)
+            if i < len(dishes) - 1:
+                i += 1
+            else:
+                break
 
-# parse the website information for the side dishe
-sides = [Dish("Gemuese/Salat"), Dish("Hauptbeilage")]
-for side in sides:
+    # parse the website information for the side dishe
+    sides = [Dish("Gem\xc3\xbcse/Salat"), Dish("Hauptbeilage")]
+    i = 0
     for line in lines[::-1]:
         sline = line.strip()
-        if sline.lower().startswith(side.category.lower()):
-            side.description = side.extract_description(sline)
-            break
+        if sline.lower().startswith(sides[i].category.lower()) or sline.lower().startswith(sides[i].category.replace("\xc3\xbc", "ue").lower()):
+            sides[i].description = sides[i].extract_description(sline)
+            if i < len(sides) - 1:
+                i += 1
+            else:
+                break
 
-# print out the information
-print bcolors.BOLD + lines[0], "\n", bcolors.ENDC
-for i in range(len(dishes)):
-    if i % 2:
-        print bcolors.OKBLUE + dishes[i].category.rjust(20), dishes[i].description.ljust(170), dishes[i].price, "Euro", bcolors.ENDC
-    else:
-        print dishes[i].category.rjust(20), dishes[i].description.ljust(170), dishes[i].price, "Euro"
-print
-for side in sides[::-1]:
-    print side.category.rjust(20), side.description.ljust(170)
-print
+    # print out the information
+    print bcolors.BOLD + lines[0], "\n", bcolors.ENDC
+    for i in range(len(dishes)):
+        if i % 2:
+            print bcolors.OKBLUE + dishes[i].category.rjust(21), dishes[i].description.ljust(80), dishes[i].price, bcolors.ENDC
+        else:
+            print dishes[i].category.rjust(21), dishes[i].description.ljust(80), dishes[i].price
+    print
+    for side in sides[::-1]:
+        print side.category.rjust(21), side.description.ljust(80)
+    print
+
+if __name__ == "__main__":
+    sys.exit(main())
